a906c86dac18844a4db74f3ac6394a9728581e20
Aleksey Goravskiy <32121103+AGoravskiy@users.noreply.github.com>
2020-10-12 16:42:30 +0300

    feat: implement decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const DOT_DASHE = {
+        '.': '10',
+        '-': '11',
+    };
+
+    const encodingSpace = '**********';
+    const dotDasheKeys = Object.keys(DOT_DASHE);
+    const dotDasheValues = Object.values(DOT_DASHE);
+    const morseTableKeys = Object.keys(MORSE_TABLE);
+    const orseTableValues = Object.values(MORSE_TABLE);
+
+    let exprPart;
+    let decodingStr = '';
+    let binaryEncodingUnit;
+    let morseEncodingUnit = '';
+
+    if (!(expr.length % 10)) {
+        expr = expr.concat(Array.of(expr.length % 10).fill(0));
+    }
+
+    for (let i = 0; i < expr.length; i += 10) {
+        exprPart = expr.slice(i, i + 10);
+        if (exprPart === encodingSpace) {
+            decodingStr += ' ';
+        } else {
+            for (let j = 0; j < exprPart.length; j += 2) {
+                binaryEncodingUnit = exprPart.slice(j, j + 2);
+    
+                if (dotDasheValues.includes(binaryEncodingUnit)) {
+                    morseEncodingUnit += dotDasheKeys[dotDasheValues.indexOf(binaryEncodingUnit)];
+                }
+            }
+        }
+        if (morseEncodingUnit) {
+            decodingStr += orseTableValues[morseTableKeys.indexOf(morseEncodingUnit)];
+        }
+
+        morseEncodingUnit = '';
+    }
+    return decodingStr;
 }
 
 module.exports = {
